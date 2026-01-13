f3d5e7a5a651d1e3ed5420b4843935a160042555
Sherry <103855706+lei-xue@users.noreply.github.com>
2026-01-13 13:03:50 -0800

    leetcode: Solved 0268-missing-number (Easy -.java )
    Date: 01-13-2026
    Time Complexity: 0 ms (100%)
    Space Complexity: 47.5 MB (36.88%)

--- LeetCode/Easy/0268-missing-number/0268-missing-number.java
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.java
@@ -1,10 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int sum = (n+1) * n/2;
+        // int n = nums.length;
+        // int sum = (n+1) * n/2;
+        // for(int i = 0; i < nums.length; i++){
+        //     sum -= nums[i];
+        // }
+        // return sum;
+        // XOR 
+        int res = nums.length;
         for(int i = 0; i < nums.length; i++){
-            sum -= nums[i];
+            res ^= i ^ nums[i];
         }
-        return sum;
+        return res;
     }
 }
