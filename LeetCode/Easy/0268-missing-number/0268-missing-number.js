739a7d58e0dfeafcde48833da17a26302e61d185
Sherry <103855706+lei-xue@users.noreply.github.com>
2026-01-13 13:05:15 -0800

    leetcode: Solved 0268-missing-number (Easy -.js )
    Date: 01-13-2026
    Time Complexity: 0 ms (100%)
    Space Complexity: 55.6 MB (92.28%)

---
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let sum = (nums.length + 1) * nums.length/2;
+    for(let i = 0; i < nums.length; i++) {
+        sum -= nums[i];
+    }
+    return sum;
+};
