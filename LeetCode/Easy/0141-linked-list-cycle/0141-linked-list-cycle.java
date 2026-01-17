e8cd6ae0e1b363ca673a3e33b5246c7626587573
Sherry <103855706+lei-xue@users.noreply.github.com>
2026-01-17 13:02:53 -0800

    leetcode: Solved 0141-linked-list-cycle (Easy -.java )
    Date: 01-17-2026
    Time Complexity: 0 ms (100%)
    Space Complexity: 46.6 MB (77.23%)

--- LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,14 +12,26 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         // use set 
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode current = head;
-        while(current != null){
-            if(set.contains(current)){
+        // HashSet<ListNode> set = new HashSet<>();
+        // ListNode current = head;
+        // while(current != null){
+        //     if(set.contains(current)){
+        //         return true;
+        //     }
+        //     set.add(current);
+        //     current = current.next;
+        // }
+        // return false;
+        // use two pointer;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
                 return true;
             }
-            set.add(current);
-            current = current.next;
+
         }
         return false;
     }
