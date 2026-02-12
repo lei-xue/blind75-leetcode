72d390c63f281f893feb39fe8799664df39280c6
Sherry <103855706+lei-xue@users.noreply.github.com>
2026-02-12 15:32:58 -0800

    leetcode: Solved 0070-climbing-stairs (Easy .java ) Date: 02-12-2026 Time Complexity: 0 ms (100%) Space Complexity: 42.4 MB (15.06%)

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        // edge case
+        if(n<=2) {
+            return n;
+        }
+        int first = 1;
+        int second = 2;
+        int result = 0;
+        for(int i = 3; i <= n; i++){
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+}
